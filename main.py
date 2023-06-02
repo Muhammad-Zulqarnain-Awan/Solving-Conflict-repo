8635583f4863cfc3156af40be66eb0b0c5f07e2f
nimra-at-786164672226 <nimraakram321@gmail.com>
2023-06-02 22:53:40 +0500

    Revert "Remove print statement from people function and add return value."
    This reverts commit abce40a7fd73d543c43e14e9586ce9159b24f837.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 def people():
     print("All the death. We are ghosts.")
     print(2+5)
@@ -6,3 +7,14 @@ def people():
     print(name)
 
 people()
+=======
+def people(number):
+    
+    if number > 0:
+        print("World Population: ",number)
+    else:
+        print("All are death. We are ghosts.")
+
+number = int(input("Enter the number of population: "))
+people(number)
+>>>>>>> parent of abce40a (Remove print statement from people function and add return value.)
